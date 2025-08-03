b37abb6ac3a2094d53194a89d978730928bbd7d8
priyanjalii04 <139577018+priyanjalii04@users.noreply.github.com>
2025-08-03 22:26:57 +0530

    Add files via upload

---
+++ Components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+   <footer className='footer'>
+    copyright &#169; priyanjali .All right received.
+   </footer>
+  )
+}
+
+export default Footer
